60b9c59a18b6ba506d6cb1d908e16aa488cb0f54
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2024-10-26 00:23:09 +0200

    add routes file api.php

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,9 @@
 <?php
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', fn() => new JsonResponse([
+    'version' => '0.0.1',
+    'phpVersion' => phpversion(),
+]));
